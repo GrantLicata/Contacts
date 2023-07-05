ad1c143d4e1b0b5df662a1cf70c79cf0812db0b7
GrantLicata <grant.licata@gmail.com>
2023-07-04 17:28:51 -0700

    Preparing the contact card

---
+++ components/contactCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const contactCard = (props) => {
+  return (
+    <div>
+      <h3>Full Name</h3>
+      <p>Email</p>
+      <p>Message</p>
+    </div>
+  );
+};
+
+export default contactCard;
