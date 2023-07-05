fb7eee2cf7ed16e418885bda3d48836e955c42f2
GrantLicata <grant.licata@gmail.com>
2023-07-04 21:04:21 -0700

    The ability to get cards from the server and display on the page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,45 @@
+"use client";
+
 import ContactCard from "@/components/contactCard";
 import ContactForm from "@/components/contactForm";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+
+  // Gather contacts upon loading of the page
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("api/contact", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        setErr(true);
+      }
+
+      const data = await res.json();
+
+      setData(data);
+      console.log(data);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="p-4 max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold">Contact Us</h1>
       <p>Please fill in the form below</p>
       <ContactForm />
-      <ContactCard />
+      {data.map((contact) => (
+        <ContactCard
+          key={contact._id}
+          fullName={contact.fullname}
+          email={contact.email}
+          message={contact.message}
+        />
+      ))}
     </div>
   );
 }
--- components/contactCard.jsx
+++ components/contactCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const ContactCard = (props) => {
   return (
     <div className="bg-slate-200  rounded-lg py-4 px-3 mt-4 border-t">
-      <h3>Full Name</h3>
-      <p>Email</p>
-      <p>Message</p>
+      <h3>{props.fullName}</h3>
+      <p>{props.email}</p>
+      <p>{props.message}</p>
     </div>
   );
 };
