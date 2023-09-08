1f819e4064e6dfb0750c3f3116a997c3e4908fe9
GrantLicata <grant.licata@gmail.com>
2023-09-07 21:07:55 -0700

    Navbar code cleaned

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,46 +16,27 @@ const Navbar = (params) => {
   return (
     <div className="flex justify-between h-14 items-center px-2">
       <h1 className="text-3xl font-bold">Contact Manager</h1>
-      {navMenu === false ? (
-        <div className="flex gap-2">
-          <Link href="/login">
-            <button className="flex items-center gap-1 bg-slate-800 text-white hover:bg-slate-800 hover:shadow-sm rounded-md px-2 py-1">
-              <p>Login</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-auto h-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-                />
-              </svg>
-            </button>
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <Link href="/profile">
+      <div className="flex gap-2">
+        <Link href="/login">
+          <button className="flex items-center gap-1 bg-slate-800 text-white hover:bg-slate-800 hover:shadow-sm rounded-md px-2 py-1">
+            <p>Login</p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              fill="none"
               viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-9 h-9 fill-slate-900 drop-shadow-lg cursor-pointer"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-auto h-4"
             >
               <path
-                fillRule="evenodd"
-                d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
               />
             </svg>
-          </Link>
-        </div>
-      )}
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
