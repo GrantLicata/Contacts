bec054b37850ca61a3688fa45c890ed7c10adb80
GrantLicata <grant.licata@gmail.com>
2023-09-11 18:10:08 -0700

    Loading animation fixed

--- app/loading/page.jsx
+++ app/loading/page.jsx
@@ -2,8 +2,8 @@ import styles from "./page.module.css";
 
 export default function Loading() {
   return (
-    <div class={styles.centerBody}>
-      <div class={styles.loaderCircle}></div>
+    <div className={styles.centerBody}>
+      <div className={styles.loaderCircle}></div>
     </div>
   );
 }
