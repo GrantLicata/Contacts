5bbe9d21ef0990389d2df75ac005694de4c8d267
GrantLicata <grant.licata@gmail.com>
2023-09-14 20:06:06 -0700

    Loading testing route removed

--- app/loading/page.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Loading() {
-  return (
-    <div className={styles.centerBody}>
-      <div className={styles.loaderCircle}></div>
-    </div>
-  );
-}
