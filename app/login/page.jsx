5b1552e6260d50025d740f8119e85d90ad9fec04
GrantLicata <grant.licata@gmail.com>
2023-09-17 09:11:18 -0700

    Cleaning up unused pacakages across app

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,6 @@
-"use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import GoogleIcon from "public/google-logo-240.png";
 import { signIn, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 
 const Login = () => {
   const handleGoogleLogin = () => {
--- components/DropMenu.jsx
+++ components/DropMenu.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function DropdownMenu() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
 
   return (
